e1ed482970c0e94b3ec5397ad1cc7836b0235d19
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-04-09 09:04:40 -0300

    Cria páginas teste

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Página de perguntas</h1>
+            <a href="/">Ir para home</a>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Hello, Next.js!</h1>
+            <a href="/faq">Ir para faq</a>
+        </div>
+    )
+}
